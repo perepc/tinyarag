9dae0b352c4c6a650ff81fc91392c8930c98bf42
Pere Pasamonte <perepc@gmail.com>
2024-07-02 12:56:27 +0200

    Fix gramatical error

--- packages/question_rewriter/tests/test_question_rewriter.py
+++ packages/question_rewriter/tests/test_question_rewriter.py
@@ -4,7 +4,7 @@ dotenv.load_dotenv()
 from packages.question_rewriter.llm import question_rewriter
 
 def test_question_rewriter():
-    question = "Which programs Potter 3000 has?"
+    question = "Which programs Potter 3000 have?"
     print(f"\n\nTesting rewrite question: {question}")
     result = question_rewriter.invoke({"question": question})
     print (f"Edited question: {result}")
